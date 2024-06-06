8bb1934505b935050375ed8f8523d6f854d7bb64
jameswhatcott <james@whatcott.com>
2024-06-06 18:51:47 -0500

    tried to create a very basic modal box to start with

---
+++ Assets/js/script.js
@@ -0,0 +1,24 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    var modal = document.getElementById("myModal");
+    var btn = document.getElementById("openModalBtn");
+    var span = document.getElementsByClassName("close")[0];
+
+    // Show the modal automatically when the page loads
+    modal.style.display = "block";
+
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+});
+

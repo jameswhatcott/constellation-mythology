82b38aaa5f6c35267733de4dd5c670d9e576c8ee
jameswhatcott <james@whatcott.com>
2024-06-10 18:35:50 -0500

    added the beginings of the weather api but didn't add the lat and lon variables

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -21,6 +21,42 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
+//--------------------- API for weather-----------------------------------------
+
+const form = document.getElementById("myForm");
+const cityInput = document.getElementById("cityInput");
+const weatherResult = document.getElementById("weatherResult");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const city = cityInput.value.trim();
+    if (city) {
+        fetchWeatherData(city);
+        localStorage.setItem("lastCity", city);
+    }
+});
+
+function fetchWeatherData(city) {
+    const lat = 
+    const lon = 
+    const url = `https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=cloud_cover&hourly=temperature_2m,precipitation_probability,precipitation,visibility&temperature_unit=fahrenheit&forecast_days=1`;
+    
+    fetch(url)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then(data => {
+                displayWeatherData(data);
+            })
+            .catch(error => {
+                console.error("Error fetching weather data:", error);
+                weatherResult.innerHTML = `<p>Error fetching weather data: ${error.message}</p>`;
+            });
+    }
+
 
 
 

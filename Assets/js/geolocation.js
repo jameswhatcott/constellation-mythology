ffbec97f188af35be5135e19889be9104f4e705b
Matthew Williams <mattthewwilliamscmh@icloud.com>
2024-06-11 13:21:23 -0400

    geolocation.js, modal.html, modal.css: these are just to help James getting the user input; have to click the button twice to get result, so not quite right yet, but will give lat and lon from geolocation or from zip

--- Assets/js/geolocation.js
+++ Assets/js/geolocation.js
@@ -1,6 +1,9 @@
 let lat = ""; // these need to be global variables to pass out of the .then statements
 let lon = "";
 
+const submitBtn = document.getElementById("submitBtn")
+
+document.getElementById("geoLocEl").checked = true
 function getLocation() {
   // I'm imagining this as a radio button with geolocation and zip-code options.
   // The zip code option also includes an input box for the user to provide a zip code.
@@ -11,23 +14,34 @@ function getLocation() {
   if (document.getElementById("geoLocEl").checked) {
     if (navigator.geolocation) { // geolocation option chosen by user
       navigator.geolocation.getCurrentPosition(showPosition);
-      lat = position.coords.latitude;
-      lon = position.coords.longitude;
     } else { // no results returned, likely because the browser does not support geolocation
       alert("Your brower does not support geolocation. Try a different browser.");
       return;
     }
   } else { // if zip-code option is chosen by the user
-  searchZip = document.getElementById("zipInput")
-  // fetch (`https://maps.googleapis.com/maps/api/geocode/json?address=${searchZip}&key=AIzaSyAkxUKm1ntgvpMBIPdmwi_lx8nib9Bfaiw`) //Matthew's Google Maps API key
-  fetch (`https://maps.googleapis.com/maps/api/geocode/json?address=43212&key=AIzaSyAkxUKm1ntgvpMBIPdmwi_lx8nib9Bfaiw`) //Matthew's Google Maps API key; zip code for testing only; use line above for variable
-  .then (function (coordinates) {
-    return coordinates.json()
-  })
-  .then (function (coordinatesObj) {
-    lat = coordinatesObj.results[0].geometry.location.lat;
-    lon = coordinatesObj.results[0].geometry.location.lng;
-  });
+    const searchZip = document.querySelector("#zipInput").value
+    if (searchZip === "") {
+      alert("Please supply a zip code.")
+      return;
+    }
+    fetch (`https://maps.googleapis.com/maps/api/geocode/json?address=${searchZip}&key=AIzaSyAkxUKm1ntgvpMBIPdmwi_lx8nib9Bfaiw`) //Matthew's Google Maps API key
+    .then (function (coordinates) {
+      return coordinates.json()
+    })
+    .then (function (coordinatesObj) {
+      lat = coordinatesObj.results[0].geometry.location.lat;
+      lon = coordinatesObj.results[0].geometry.location.lng;
+    });
+  };
+  console.log(lat + ", " + lon)
 }
 
-getLocation();
+function showPosition(position) {
+  lat = position.coords.latitude;
+  lon = position.coords.longitude;
+}
+
+
+//need an event listner that changes radio button to postal code option 
+// if anything entered into zip code input
+submitBtn.addEventListener("click", getLocation);

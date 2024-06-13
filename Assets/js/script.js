6054f0799a0df7fc6ed12f56092429b6417ca405
jameswhatcott <james@whatcott.com>
2024-06-13 12:03:04 -0500

    still playing with display, not done yet

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -6,7 +6,7 @@ let zipCode = params.get("zipcode");
 
 function fetchWeatherData(lat, lon) {
     
-    const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=cloud_cover&hourly=temperature_2m,precipitation_probability,precipitation,visibility&temperature_unit=fahrenheit&forecast_days=1`;
+    const url = `https://api.open-meteo.com/v1/forecast?latitude=31.328358&longitude=-94.653306&current=temperature_2m,cloud_cover&hourly=temperature_2m,apparent_temperature,precipitation_probability,precipitation,visibility&temperature_unit=fahrenheit&timezone=America%2FChicago&forecast_days=1`;
     
     fetch(url)
         .then(response => {
@@ -31,11 +31,23 @@ function fetchWeatherData(lat, lon) {
 }
 
 function displayWeather(data) {
+    const todayTime = document.querySelector(".time");
     const todayTemp = document.querySelector("#todayWeather .temp");
     const todayCloudCover = document.querySelector("#todayWeather .cloudCover");
     const todayVisibility = document.querySelector("#todayWeather .visibility");
 
-    todayTemp.innerHTML += `${data.hourly.temperature_2m[0]} ${data.hourly_units.temperature_2m}`;
+   
+    function convertISOToNormalTime(isoTime) {
+        const date = new Date(isoTime);
+        const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', timeZoneName: 'short' };
+        return date.toLocaleString('en-US', options);
+    }
+    
+    const isoTime = data.current.time;
+    const normalTime = convertISOToNormalTime(isoTime);
+
+    todayTime.innerHTML = normalTime;
+    todayTemp.innerHTML += `${data.current.temperature_2m} ${data.current_units.temperature_2m}`;
     todayCloudCover.innerHTML += `${data.current.cloud_cover} ${data.current_units.cloud_cover}`;
 
     if (data.hourly.visibility[0] < 1000) {
@@ -44,7 +56,7 @@ function displayWeather(data) {
         todayVisibility.innerHTML += ' Clear ';
     }
 
-    for (let i = 1; i < 6; i++) {
+    for (let i = 1; i < 7; i++) {
         const hrTemp = document.querySelector(`#hrWeather\\[${i}\\] .temp`);
         const hrCloudCover = document.querySelector(`#hrWeather\\[${i}\\] .cloudCover`);
         const hrVisibility = document.querySelector(`#hrWeather\\[${i}\\] .visibility`);
@@ -56,8 +68,8 @@ function displayWeather(data) {
             hrCloudCover.innerHTML += `${data.current.cloud_cover} ${data.current_units.cloud_cover}`;
         }
 
-        if (data.hourly.visibility[i] < 1000) {
-            if (hrVisibility) {
+        if (hrVisibility) {
+            if (data.hourly.visibility[i] < 1000) {
                 hrVisibility.innerHTML += ' Foggy ';
             } else {
                 hrVisibility.innerHTML += ' Clear ';
